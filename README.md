9cea330c2032d01400f53928e21346125e4fa0d8
Sawan <ssmahara96@gmail.com>
2021-04-21 14:31:35 +0000

    True initial commit

--- README.md
+++ README.md
@@ -1,2 +1,159 @@
 # Bulk-Email
-Repository to send bulk emails via python.
+
+This repository contains a simple python script that can send emails to multiple individuals. This code was built to primarily solve the problem of sending grades and scores of students individually. 
+
+It is suggested to run the script with 
+
+```bash
+python email.py --test_mode True
+```
+
+to run it in test mode and not send any emails, but run through everything else. More details about test mode are detailed below.
+
+## Usage with no attachments
+
+The `Summary.xlsx`file with multiple sheets gives is for reference. It gives an idea of how to arrange information and sending emails to individuals whose email and names are present in `email_names.csv`. 
+
+You can run the code by executing 
+
+``` bash
+python email.py
+```
+
+An example message body 
+
+```
+Hello AA,
+ Please find attached a .txt file that summarises your grades and scores for the <INSERT COURSE HERE> course.
+ It is suggested to open the document in landscape mode, if you are having trouble viewing it.
+
+ Thank you
+
+ Regards,
+ Sawan Singh Mahara 
+```
+
+The script uses the details entered in `Summary.xlsx` and creates a `.txt ` file that will be attached to the email. It can be found in the `Text Files` folder.
+
+## Usage with attachments and no xlsx file.
+
+As an example, if you want to send a `PDF` to each individual, make sure that the `PDF` is named `<INDIVIDUAL EMAIL>.pdf` where for example if you want to send the email to `xyz@abc.com`, the pdf should be `xyz.pdf`. They must be put into the `Attachments `folder.
+
+You can run the code by executing 
+
+``` bash
+python email.py --use_xlsx False --attachment_extention .pdf
+```
+
+The arguments `--use_xlsx` and `--attachment_extention` can be combined with other arguments as well. You can type `python email.py --help`  to know about the other supported arguments.
+
+An example message body 
+
+```
+Hello AA,
+ Please find attached a PDF for you.
+
+ Thank you
+
+ Regards,
+ Sawan Singh Mahara 
+```
+
+The script uses the details entered in `Summary.xlsx` and creates a `.txt ` file that will be attached to the email. It can be found in the `Text Files` folder.
+
+## Test Mode
+
+If you want to run the script in a test mode not risking accidentally sending the wrong file to the wrong recipient, you can run the script as
+
+``` bash
+python email.py --test_mode True
+```
+
+It will prompt you to enter some test emails, that you don't mind sending it to, for example it can be your own email address again or trusted party's email address.
+
+This will make the script perform the exact same way, but instead of sending the email to every recipient, it randomly selects an individual from the list of individuals in `email_names.csv` generates the necessary email body/attachments and sends their intended email to the test email addresses.
+
+# email_names.csv
+
+If you want to send emails to the individuals aaa@iitdh.ac.in, bbb@iitdh.ac.in,...., zzz@iitdh.ac.in, this `.csv` file is already setup.
+
+If not, you can modify the `.csv` file or create your own in the same format and replace it here.
+
+# Summary.xlsx
+
+For the provided Summary.xlsx file, the corresponding `.txt` file for each individual is created and stored in `Text Files`. The contents of this folder can be deleted and your own `Summary.xlsx` file can be used as well. 
+
+The script only works if there are exactly two sheets. If you need only one sheet or more than 2 sheets, you will have to modify the code.
+
+The following lines in the code control the sheets.
+
+```python
+#%% XLSX Sheet Code
+Email_names=np.loadtxt('email_names.csv',dtype=str,delimiter=',')
+if use_xlsx:
+    sheet1=pd.read_excel("Summary.xlsx",sheet_name=0)
+    sheet2=pd.read_excel("Summary.xlsx",sheet_name=1)
+    sheets=[sheet1,sheet2]
+```
+
+and
+
+```python
+for work in ["Sheet 1 Title", "Sheet 2 Title"]:
+    a=sheets[itr].iloc[individual,2:] #Consider the cells from column C onward, row 'itr'
+    a=a.to_string()+'\n \n \n'
+```
+
+
+
+### If there is only one sheet use
+
+```python
+#%% XLSX Sheet Code
+Email_names=np.loadtxt('email_names.csv',dtype=str,delimiter=',')
+if use_xlsx:
+    sheet1=pd.read_excel("Summary.xlsx",sheet_name=0)
+    sheets=[sheet1]  
+```
+
+and
+
+```python
+for work in ["Sheet 1 Title"]:
+    a=sheets[itr].iloc[individual,2:] #Consider the cells from column C onward, row 'itr'
+    a=a.to_string()+'\n \n \n'  
+```
+
+
+
+### If there are four sheets, use
+
+```python
+#%% XLSX Sheet Code
+Email_names=np.loadtxt('email_names.csv',dtype=str,delimiter=',')
+if use_xlsx:
+    sheet1=pd.read_excel("Summary.xlsx",sheet_name=0)
+    sheet2=pd.read_excel("Summary.xlsx",sheet_name=1)
+    sheet3=pd.read_excel("Summary.xlsx",sheet_name=2)
+    sheet4=pd.read_excel("Summary.xlsx",sheet_name=3)
+    sheets=[sheet0,sheet1,sheet2,sheet3]
+```
+
+and
+
+``` python
+for work in ["Sheet 1 Title", "Sheet 2 Title","Sheet 3 Title", "Sheet 4 Title"]:
+    a=sheets[itr].iloc[individual,2:] #Consider the cells from column C onward, row 'itr'
+    a=a.to_string()+'\n \n \n'  
+```
+
+If there are different number of sheets, modify these lines accordingly. 
+
+# Google Authentication
+
+If the sending email is a google email, the script will work only if two-factor authentication is not enabled. 
+
+If it is, then you have to head over to https://myaccount.google.com/lesssecureapps and enable less secure apps to use your account.
+
+Once you have used the script, you can disable this to get back security.
+
---
+++ email.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  5 09:10:03 2020
+
+@author: mahara
+"""
+import smtplib
+import pandas as pd
+import numpy as np
+# from fpdf import FPDF 
+# from time import sleep
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+def mysendmail(toaddrs,final_msg,**kwargs):
+    roll=kwargs.get('roll',-1)
+    name=kwargs.get("name","")
+    fromaddr = kwargs.get('fromaddr','@iitdh.ac.in')
+    password=kwargs.get('password', 1234)
+    toaddr = toaddrs
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = ",".join(toaddr)
+    msg['Cc']=",".join(cc_addresses)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(final_msg, 'plain'))
+    filename = kwargs.get('attachment',None)
+    if type(filename)!=type(None):
+        attachment = open(filename, "rb")
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+        msg.attach(part)
+    
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    try:
+        if not test_mode:
+            server.login(fromaddr, password)
+        text = msg.as_string()
+        toadd=[toaddr]+cc_addresses+bcc_addresses
+        if not test_mode:
+            server.sendmail(fromaddr, toaddr, text)
+            server.quit()
+        if not test_mode:
+            print(" Sent mail to "+name, toaddr)
+        else:
+            print(f" Test mode enabled. Did not send email intended for {name} {toaddr}")
+    except Exception as e:
+        if e.smtp_code==535:
+            print('\n Couldn\'t send mail, try disabling secure authentication for your account here \n https://myaccount.google.com/lesssecureapps \n Ensure that the correct Gmail account is selected while you turn this off. \n You can turn it back on after sending emails')
+        else:
+            print("\n Couldn't send mail. Got error Code ", e.smtp_code)
+#%% Imports
+import getpass
+import argparse
+#%% Getting user info
+parser = argparse.ArgumentParser(description='Emailing in Bulk.')
+parser.add_argument("--cc",required=False,type=lambda x: (str(x).lower() == 'true'),default=False, help="If you want to cc individuals")
+parser.add_argument("--bcc",required=False,type=lambda x: (str(x).lower() == 'true'),default=False, help="If you want to bcc individuals. Note that this is not tested, so use at your own risk.")
+parser.add_argument("--attachment",required=False,type=lambda x: (str(x).lower() == 'true'),default=False, help="If you want to attach files.")
+parser.add_argument("--test_mode",required=False,type=lambda x: (str(x).lower() == 'true'),default=False, help="If you want run in test mode and send random individual's mails to some predesignated test emails.")
+parser.add_argument("--use_xlsx",required=False,type=lambda x: (str(x).lower() == 'false'),default=True, help="If you want to convert the program to read the xlsx file and convert it to a .txt attachment. By default it is True.")
+parser.add_argument("--attachment_extention",required=False,type=str,default='', help="The attachment extention.")
+
+
+args = parser.parse_args()
+cc=args.cc
+bcc=args.bcc
+use_attachment=args.attachment
+use_xlsx=args.use_xlsx
+test_mode=args.test_mode
+attachment_extention=parser.attachment_extention
+
+subject=input("Enter the email subject. Type -1 to use the default text 'Testing' \n")
+if subject=="-1":
+    subject='testing'
+#CC part
+cc_counter=0
+cc_addresses=[]
+starting_cc=True
+while cc:
+    if starting_cc:
+        address=input(f"Enter the address cc recipient {cc_counter}. When you are done typing, press enter twice. \n")
+        starting_cc=False
+    else:
+        address=input()
+    if address:
+        cc_addresses.append(address)
+    else:
+        break
+    cc_counter+=1
+#BCC part
+bcc_counter=0
+bcc_addresses=[]
+starting_bcc=True
+while bcc:
+    if starting_bcc:
+        address=input(f"Enter the address bcc recipient {bcc_counter}. When you are done typing, press enter twice. \n")
+        starting_bcc=False
+    else:
+        address=input()
+    if address:
+        bcc_addresses.append(address)
+    else:
+        break
+    bcc_counter+=1
+
+# TEST EMAIL
+test_counter=0
+test_addresses=[]
+starting_test=True
+while test_mode:
+    if starting_test:
+        address=input(f"Enter test email address of recipient {test_counter}. When you are done typing, press enter twice. \n")
+        starting_test=False
+    else:
+        address=input()
+    if address:
+        test_addresses.append(address)
+    else:
+        break
+    test_counter+=1
+
+
+if not test_mode:
+    your_email_id=input(" Enter Your Email Address\n")
+    your_password=getpass.getpass(" Enter your password. Whatever you enter will not be visible.\n")
+else:
+    your_email_id="xyz@email.com"
+    your_password=1234 #If running in test mode, the dummy password is 1234
+lines = []
+
+#%%Greeting
+greeting=input("Type in the greeting to use for everyone. Their name will be added to this, followed by a comma.\n")
+#%% MAIN BODY
+starting_main_body=True
+while True:
+    if starting_main_body:
+        line=input(" Type in the main body of text that you would like to give everyone, excluding the greetings and salutations.\n When you are done typing, press enter twice. \n")
+        starting_main_body=False
+    else:
+        line=input()
+    if line:
+        lines.append(line)
+    else:
+        break
+common_body = '\n'.join(lines)
+#%% ENDING SALUTATIONS
+starting_salutations=True
+while True:
+    if starting_salutations:
+        line=input(" Type in ending salutations you would like to give everyone. \n When you are done typing, press enter twice. \n")
+        starting_salutations=False
+    else:
+        line=input()
+    if line:
+        lines.append(line)
+    else:
+        break
+common_body_with_salutation= '\n'.join(lines)
+
+
+#%% XLSX Sheet Code
+Email_names=np.loadtxt('email_names.csv',dtype=str,delimiter=',')
+if use_xlsx:
+    sheet1=pd.read_excel("Summary.xlsx",sheet_name=0)
+    sheet2=pd.read_excel("Summary.xlsx",sheet_name=1)
+    sheets=[sheet1,sheet2]
+    # na_filler=-12345678
+    for sheet in sheets:
+        sheet.dropna()
+        # sheet.fillna(na_filler,inplace=True)
+    #     sheet.replace("Not Applicable",na_filler,inplace=True)
+#%% MAIN
+pd.set_option('display.max_colwidth', None)
+# test_mail=['191081003@iitdh.ac.in','191081002@iitdh.ac.in']
+
+for individual in np.random.randint(len(Email_names)) if test_mode else range(len(Email_names)):
+# for individual in [4]:
+    if use_xlsx:
+        name=Email_names[individual,1]
+        roll=str(int(Email_names[individual,0]))
+        email=test_addresses if test_mode else roll+["@iitdh.ac.in"]
+        """xlsx to Text File generation """
+        #From the first sheet, extract cells A1 and B1
+        mssg1=sheets[0].iloc[individual,0:2].to_string()+"\n \n \n \n" 
+        itr=0
+        for work in ["Quiz 1", "Quiz 2"]:
+            a=sheets[itr].iloc[individual,2:] #Consider the cells from column C onward, row 'itr'
+            a=a.to_string()+'\n \n \n'
+            mssg1=mssg1+work.center(60,'-')+'\n'+a
+            itr=itr+1
+        textfile = open("Text Files/"+roll+".txt", 'w')
+        textfile.write(mssg1)
+        textfile.close()
+    f=1
+    body=f"{greeting} {name},\n {common_body_with_salutation}"
+    mysendmail(email,
+                    body,
+                    name=name,
+                    fromaddr=your_email_id,
+                    password=your_password,
+                    attachment=None if attachment_extention!='' else f"Attachments/{roll}{attachment_extention}"
+            )
